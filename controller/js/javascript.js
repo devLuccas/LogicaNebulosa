12545e9468fb4ee83bea26e861e24dfd81723307
Luccas.Goncalves <lucgmelo@hotmail.com>
2019-10-24 10:39:09 -0300

    Commit teste

--- controller/js/javascript.js
+++ controller/js/javascript.js
@@ -1,63 +1,205 @@
 const OPTS = ['.', '.', '.', '.', 'O'];
 const CONFIG = {
-    size: 15,
-    inteval: 2
+    size: 10,
+    interval: 5,
 };
+const MAPPER = {
+    close: 0,
+    medium: 1,
+    far: 2
+}
 
-let ROUND = 0;
+let ROUND = 1;
 let MAP = [[]];
 let FINISH = false;
+let ROBO = [0, 0];
+let TESTE = [0, 0];
 
 async function iniciar() {
+
     MAP = getMap();
     //Setando posicao inicial Robô
-    setPosition(0, 0, '@');
+    setPosition(ROBO[0], ROBO[1], '@');
     //Setando posicao da saida
     setPosition(CONFIG.size - 1, CONFIG.size - 1, 'S');
     print(MAP);
+
     while (!FINISH) {
+        console.log('ROBO: ', ROBO);
+        finish();
         await go();
     }
 
-
-
-    print(MAP);
+    console.log('VOCE ENCONTROU A SAÍDA');
 }
 
 async function go() {
     ROUND++;
-    let robotPosition = await getRobotPosition();
-    console.log(robotPosition);
-    const next = getNext();
+    const next = await getNext();
+    // setPosition(4, 4, '@');
     setPosition(next[0], next[1], '@');
-    setPosition(robotPosition[0, robotPosition[1], '.');
     print(MAP);
-    if (ROUND === 2) {
-        FINISH = true;
+}
+
+async function getNext() {
+    let rr = ROBO[0];
+    let cr = ROBO[1];
+    const coordinate = {
+        top: getTop(rr, cr),
+        right: getRight(rr, cr),
+        bottom: getBottom(rr, cr),
+        left: getLeft(rr, cr),
     }
-    
+    console.log('cordinante: ', coordinate)
+    const fuzzy = await fuzzyfication(coordinate);
+    const distance = await getDistance(fuzzy);
+    const step = await defuzzyfication(distance);
+    return step;
 }
 
-function getNext() {
-    //regras Nebulosas
-    CONFIG.inteval;
-    return [0, 1]
+function getRight(r, c) {
+    try {
+        // console.log('********************')
+        const RIGHT = [];
+        let right;
+        const interval = getInterval(c);
+
+        for (let i = 0; i <= interval; i++) {
+            RIGHT.push(MAP[r][c + 1 + i]);
+        }
+
+        for (let [index, element] of RIGHT.entries()) {
+            if (element === 'O') {
+                right = index;
+                break;
+            }
+
+            right = index;
+        }
+        // console.log('right', right)
+        // console.log('********************')
+        return right
+    } catch (error) { FINISH = true
+        console.log('GETRIGHT', error)
+        // console.log('********************')
+    }
 }
 
+function getBottom(r, c) {
+    try {
+        // console.log('********************')
+        const BOTTOM = [];
+        let bottom;
+        const interval = getInterval(r);
 
-function Nebulosa() {
-    let direita = math.max(MpX(), MmX(), MlX());
-    let baixo = math.max(MpX(), MmX(), MlX());
-    let cima = math.max(MpX(), MmX(), MlX());
-    let esquerda = math.max(MpX(), MmX(), MlX());
+        for (let i = 0; i < interval; i++) {
+            BOTTOM.push(MAP[r + 1 + i][c]);
+        }
+
+        for (let [index, element] of BOTTOM.entries()) {
+            if (element === 'O') {
+                bottom = index;
+                break;
+            }
+
+            bottom = index;
+        }
+        // console.log('bottom', bottom)
+        // console.log('********************')
+        return bottom
+    } catch (error) { FINISH = true
+        console.log('GETBOTTOM', error)
+        // console.log('********************')
+    }
 }
 
+function getTop(r, c) {
+    try {
+        // console.log('********************')
+        const TOP = [];
+        let top;
 
-async function getRobotPosition() {
+        const interval = getInverseInterval(r);
+
+        for (let i = -1; i <= interval; i++) {
+            TOP.push(MAP[r + 1 - i][c]);
+        }
+
+        for (let [index, element] of TOP.entries()) {
+            if (element === 'O') {
+                top = index;
+                break;
+            }
+
+            top = index;
+        }
+        // console.log('top', top)
+        // console.log('********************')
+        return top
+    } catch (error) { FINISH = true
+        console.log('GETTOP', error)
+        // console.log('********************')
+    }
+
+}
+
+function getLeft(r, c) {
+    try {
+        // console.log('********************')
+        const LEFT = [];
+        let left = 0;
+        const interval = getInverseInterval(c);
+        for (let i = 0; i <= interval; i++) {
+            LEFT.push(MAP[r][c - i]);
+            console.log('LEFT', LEFT);
+        }
+
+        for (let [element, index] of LEFT.entries()) {
+            if (element === 'O' || interval === 0) {
+                left = index;
+                break;
+            }
+            console.log('index: ', index);
+            left = index;
+        }
+        // console.log('left', left)
+        // console.log('********************')
+        return left
+    } catch (error) { FINISH = true
+        console.log('GETLEFT', error)
+        // console.log('********************')
+    }
+
+}
+
+function getInterval(positionRobot) {
+    const interval = CONFIG.interval + positionRobot + 1 >= CONFIG.size
+        ? CONFIG.size - (positionRobot + 1)
+        : CONFIG.interval;
+
+    return interval;
+}
+
+function getInverseInterval(positionRobot) {
+    const interval = (CONFIG.interval * (-1)) + positionRobot <= 0
+        ? positionRobot
+        : CONFIG.interval;
+
+    return interval;
+}
+
+function finish() {
+    const isFinish = ROBO[0] === CONFIG.size - 1
+        && ROBO[1] === CONFIG.size - 1;
+
+    FINISH = isFinish;
+}
+
+function getRobotPosition() {
     const arr = MAP.map((row, r) => row.map((column, c) => {
         if (column === '@') {
             return [r, c];
-        }
+        };
     }));
 
     return arr[0][0];
@@ -79,6 +221,13 @@ function print(matriz) {
 function setPosition(row, column, element) {
     MAP[row][column] = element;
 
+    if (element === '@') {
+        if ((ROBO[0] !== 0 || ROBO[1] !== 0 || ROUND !== 1)) {
+            MAP[ROBO[0]][ROBO[1]] = '.';
+        }
+        ROBO[0] = row
+        ROBO[1] = column;
+    }
 }
 
 function getMap() {
@@ -104,30 +253,133 @@ function getRow() {
     return row;
 }
 
-function MpX() {
-    if (x < 1) {
+async function fuzzyfication(params) {
+
+    const distance = {
+        top: {
+            close: maximumClose(params.top),
+            medium: maxiumMedium(params.top),
+            far: maximumFar(params.top),
+        },
+        right: {
+            close: maximumClose(params.right),
+            medium: maxiumMedium(params.right),
+            far: maximumFar(params.right),
+        },
+        bottom: {
+            close: maximumClose(params.bottom),
+            medium: maxiumMedium(params.bottom),
+            far: maximumFar(params.bottom),
+        },
+        left: {
+            close: maximumClose(params.left),
+            medium: maxiumMedium(params.left),
+            far: maximumFar(params.left)
+        }
+    }
+    console.log(distance)
+    return distance;
+}
+
+async function defuzzyfication(distance) {
+
+    if (MAPPER[distance.right] === MAPPER.far) {
+        return [ROBO[0] + 0, ROBO[1] + 5];
+    } else {
+        if (MAPPER[distance.right] === MAPPER.medium && MAPPER[distance.bottom] === MAPPER.medium) {
+            return [ROBO[0] + 0, ROBO[1] + 2];
+        } else {
+            if (MAPPER[distance.right] === MAPPER.medium && MAPPER[distance.bottom] === MAPPER.far) {
+                return [ROBO[0] + 0, ROBO[1] + 2];
+            } else {
+                if (MAPPER[distance.right] === MAPPER.close && MAPPER[distance.bottom] === MAPPER.far) {
+                    return [ROBO[0] + 5, ROBO[1] + 0];
+                } else {
+                    if (MAPPER[distance.right] === MAPPER.close && MAPPER[distance.bottom] === MAPPER.medium) {
+                        return [ROBO[0] + 2, ROBO[1] + 0];
+                    } else {
+                        if (MAPPER[distance.right] === MAPPER.close && MAPPER[distance.bottom] === MAPPER.close && MAPPER[distance.left] === MAPPER.far) {
+                            return [ROBO[0] + 0, ROBO[1] - 2];
+                        } else {
+                            if (MAPPER[distance.right] === MAPPER.close && MAPPER[distance.bottom] === MAPPER.close && MAPPER[distance.left] === MAPPER.medium) {
+                                return [ROBO[0] + 0, ROBO[1] - 1];
+                            } else {
+                                if (MAPPER[distance.right] === MAPPER.close && MAPPER[distance.bottom] === MAPPER.close && MAPPER[distance.left] === MAPPER.close && MAPPER[distance.top] === MAPPER.far) {
+                                    return [ROBO[0] - 2, ROBO[1] + 0];
+                                } else {
+                                    if (MAPPER[distance.right] === MAPPER.close && MAPPER[distance.bottom] === MAPPER.close && MAPPER[distance.left] === MAPPER.close && MAPPER[distance.top] === MAPPER.medium) {
+                                        return [ROBO[0] - 1, ROBO[1] - 0]
+                                    } else {
+                                        console.log('deu merda')
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    console.log('distances', distance);
+}
+
+async function getDistance(distance) {
+    const cartesian = {
+        top: '',
+        right: '',
+        bottom: '',
+        left: ''
+    }
+
+    Object.keys(cartesian).map(key => {
+        Object.keys(distance[key]).map(subKey => {
+            if (distance[key][subKey]) {
+                cartesian[key] = subKey;
+            }
+        })
+    });
+
+    return cartesian;
+}
+
+function getPercent() {
+    const a = CONFIG.interval * 0.2;
+    const b = CONFIG.interval * 0.4;
+    const c = CONFIG.interval * 0.8;
+    const d = CONFIG.interval;
+
+    return { a, b, c, d };
+}
+
+function maximumClose(x) {
+    const { a, b } = getPercent();
+
+    if (x < a) {
         return 1;
     } else {
-        if (1 < x < 3) {
-            return (3 - x) / 2;
+        if (x >= a && x <= b) {
+            return ((b - x) / (b - a));
         } else {
             return 0;
         }
     }
 }
 
-function MmX() {
-    if (x < 1) {
+function maxiumMedium(x) {
+    const { a, b, c, d } = getPercent();
+
+    if (x < a) {
         return 0;
     } else {
-        if (1 < x < 3) {
-            return (x - 1) / 2;
+        if (x >= a && x <= b) {
+            return ((x - a) / (b - a));
         } else {
-            if (3 < x < 4) {
+            if (x >= b && x <= c) {
                 return 1;
             } else {
-                if (4 < x < 5) {
-                    return (5 - x) / 1;
+                if (x >= c && x <= d) {
+                    return ((d - x) / (d - c));
                 } else {
                     return 0;
                 }
@@ -136,13 +388,17 @@ function MmX() {
     }
 }
 
-function MlX() {
-    if (x < 4) {
+function maximumFar(x) {
+    const { c, d } = getPercent();
+
+    if (x < c) {
         return 0;
-    } if (4 < x < 5) {
-        return (x - 4);
     } else {
-        return 1;
+        if (x >= c && x <= d) {
+            return ((x - c) / (d - c));
+        } else {
+            return 1;
+        }
     }
 }
 
@@ -159,4 +415,4 @@ console.log(`------ DICAS ------
 -------------------`);
 
 // _        _
-//   \_(ツ)_/
+//  \_(ツ)_/
